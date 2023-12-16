eb4d941b0c41dc49c3b3f77297c62ad338f49ede
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-16 13:21:22 +0300

    Class restaurant

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -1,2 +1,15 @@
 class Restaurant:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return f'The {self.name}'
+
+    def set_name(self, name):
+        self.name = name
+
+restaurant1 = Restaurant("White House")
+print(restaurant1.get_name()) # Output: The White House
+
+restaurant1.set_name("Green House")
+print(restaurant1.get_name()) # Output: Green House
