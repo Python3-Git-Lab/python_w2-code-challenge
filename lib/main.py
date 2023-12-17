395553d780888f2f04ae03f7afbcd5d162da571b
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-17 14:10:57 +0300

    Tests added

--- lib/main.py
+++ lib/main.py
@@ -4,31 +4,37 @@ from customer import Customer
 from restaurant import Restaurant
 from review import Review
 
-# Example Usage:
+class Customer:
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+
+# Creating instances of Customer 1 and Restaurant 1
 customer1 = Customer("John", "Doe")
-restaurant1 = Restaurant("Sample Restaurant")
-customer1.add_review(restaurant1, 4)
-print(Restaurant.average_star_rating(restaurant1))
-print(Customer.num_reviews(customer1))
-print(Customer.find_by_name("John Doe"))
-print(Customer.find_all_by_given_name("John"))
+restaurant1 = Restaurant("Delicious Bites")
 
-# # Sample Usage
-# customer1 = Customer("John", "Doe")
-# customer2 = Customer("Alice", "Smith")
+#customer 2 and restaurant 2
+customer2 = Customer("Jane", "Ann")
+restaurant2 = Restaurant("White House")
 
-# # print(customer1.full_name())
-# restaurant1 = Restaurant("Best Bites")
-# restaurant2 = Restaurant("Tasty Grill")
+# Creating instances of Review
+review1 = Review(customer1, restaurant2, 4)
+review2 = Review(customer1, restaurant1, 5)
 
-# review1 = Review(customer1, restaurant1, 4)
-# review2 = Review(customer2, restaurant1, 5)
-# review3 = Review(customer1, restaurant2, 3)
+# Creating instances of Review
+review3 = Review(customer2, restaurant1, 4)
+review4 = Review(customer2, restaurant2, 5)
 
-# # Accessing Data
-# print(Customer.all())  # [customer1, customer2]
-# print(Restaurant.all())  # [restaurant1, restaurant2]
-# print(Review.all())  # [review1, review2, review3]
-# print(customer1.reviews)  # [review1, review3]
-# print(restaurant1.reviews)  # [review1, review2]
+# Retrieve all reviews
+reviews = Review.get_reviews()
+
+for review in reviews:
+    print(f"Customer: {review.customer.given_name} {review.customer.family_name}")
+    print(f"Restaurant: {review.restaurant.name}")
+    print(f"Rating: {review.get_rating()}")
+    print("------")
 
--- lib/review.py
+++ lib/review.py
@@ -7,20 +7,19 @@ class Review:
         self.rating = rating
         Review.reviews.append(self)
 
-    def rating(self):
+    @classmethod
+    def get_reviews(cls):
+        return cls.reviews
+    
+    def get_rating(self):
         return self.rating
 
-    @classmethod
-    def all(cls):
-        return cls.reviews
-
     def customer(self):
-        return self.customer
+        return self._customer
 
     def restaurant(self):
-        return self.restaurant
+        return self._restaurant
 
 # # customer1 = Customer("John", "Smith")
 # # review = Review(customer1, "White house" , )
 # # print(review.rating())
-
